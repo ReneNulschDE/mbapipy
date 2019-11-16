3be95c72270eb2fdebbce48e3da2d6f639045856
Rene Nulsch <ReneNulschDE.github@Nulsch.de>
2019-11-16 10:59:47 +0100

    Add const.py, Linter

--- custom_components/mercedesmeapi/__init__.py
+++ custom_components/mercedesmeapi/__init__.py
@@ -6,230 +6,42 @@ https://home-assistant.io/components/mercedesme/
 """
 import logging
 from datetime import timedelta
-import urllib.parse
-import base64
-import time
 
-import requests
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
-from homeassistant.core import callback
-from homeassistant.components.http import HomeAssistantView
 from homeassistant.const import (
-    CONF_SCAN_INTERVAL, LENGTH_KILOMETERS,
-    CONF_EXCLUDE, CONF_USERNAME, CONF_PASSWORD)
+    CONF_SCAN_INTERVAL, CONF_USERNAME, CONF_PASSWORD)
 from homeassistant.helpers import discovery
-from homeassistant.helpers.dispatcher import (
-    async_dispatcher_connect, dispatcher_send)
+from homeassistant.helpers.dispatcher import dispatcher_send
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import track_time_interval
 
-DEPENDENCIES = ['http']
+from .apicontroller import Controller
+from .oauth import MercedesMeOAuth
+
+# DEPENDENCIES = ["http"]
 
 _LOGGER = logging.getLogger(__name__)
 
-BINARY_SENSORS = {
+DEFAULT_CACHE_PATH = ".mercedesme-token-cache"
 
-    'warningenginelight': ['Engine Light Warning',
-                           None,
-                           'binarysensors',
-                           'warningenginelight',
-                           'value',
-                           None,
-                           {
-                               'warningbrakefluid',
-                               'warningwashwater',
-                               'warningcoolantlevellow',
-                               'warninglowbattery'}],
+CONF_COUNTRY_CODE = "country_code"
+CONF_ACCEPT_LANG = "accept_lang"
+CONF_PIN = "pin"
+CONF_EXCLUDED_CARS = "excluded_cars"
+CONF_SAVE_CAR_DETAILS = "save_car_details"
 
-    'parkbrakestatus': ['Park Brake Status',
-                        None,
-                        'binarysensors',
-                        'parkbrakestatus',
-                        'value',
-                        None,
-                        {
-                            'preWarningBrakeLiningWear'}],
-
-    'windowsClosed': ['Windows Closed',
-                      None, 'windows',
-                      'windowsClosed',
-                      'value',
-                      None,
-                      {
-                          "windowstatusrearleft",
-                          "windowstatusrearright",
-                          "windowstatusfrontright",
-                          "windowstatusfrontleft",}],
-
-    'tirewarninglamp': ['Tire Warning',
-                        None,
-                        'tires',
-                        'tirewarninglamp',
-                        'value',
-                        None,
-                        {
-                            "tirepressureRearLeft",
-                            "tirepressureRearRight",
-                            "tirepressureFrontRight",
-                            "tirepressureFrontLeft",
-                            "tirewarningsrdk",
-                            "tirewarningsprw"
-                            "tireMarkerFrontRight",
-                            "tireMarkerFrontLeft",
-                            "tireMarkerRearLeft",
-                            "tireMarkerRearRight",
-                            "tireWarningRollup",
-                            "lastTirepressureTimestamp"}]
-}
-
-ODOMETER_OPTIONS = ["odo",
-                    "distanceReset",
-                    "distanceStart",
-                    "averageSpeedReset",
-                    "averageSpeedStart",
-                    "distanceZEReset",
-                    "drivenTimeZEReset",
-                    "drivenTimeReset",
-                    "drivenTimeStart",
-                    "ecoscoretotal",
-                    "ecoscorefreewhl",
-                    "ecoscorebonusrange",
-                    "ecoscoreconst",
-                    "ecoscoreaccel",
-                    "gasconsumptionstart",
-                    "gasconsumptionreset",
-                    "gasTankRange",
-                    "gasTankLevel",
-                    "liquidconsumptionstart",
-                    "liquidconsumptionreset",
-                    "liquidRangeSkipIndication",
-                    "rangeliquid",
-                    "serviceintervaldays",
-                    "tanklevelpercent",
-                    "tankReserveLamp"]
-
-LOCKS = {
-    'lock': ['Lock', None, "doors", 'locked', 'value', 'remote_door_lock'],
-}
-
-SWITCHES = {
-    'aux_heat': ['AUX HEAT', None,
-                 'auxheat', 'auxheatActive',
-                 'value', 'aux_heat', None, 'heater'],
-
-    'climate_control': ['CLIMATE CONTROL', None, 'precond',
-                        'preconditionState', 'value',
-                        'charging_clima_control', None, 'climate'],
-}
-
-SENSORS = {
-    'lock': ['Lock', None, "doors", 'locked', 'value', None,
-             {
-                 'fuelLidClosed',
-                 'doorStateFrontLeft',
-                 'doorStateFrontRight',
-                 'doorStateRearLeft',
-                 'doorStateRearRight',
-                 'frontLeftDoorLocked',
-                 'frontRightDoorLocked',
-                 'rearLeftDoorLocked',
-                 'rearRightDoorLocked',
-                 'frontLeftDoorClosed',
-                 'frontRightDoorClosed',
-                 'rearLeftDoorClosed',
-                 'rearRightDoorClosed',
-                 'rearRightDoorClosed',
-                 'doorsClosed',
-                 'trunkStateRollup',
-                 'sunroofstatus',}],
-
-    'rangeElectricKm' : ['Range electric', 'Km', "electric", 'rangeElectricKm',
-                         'value', 'charging_clima_control',
-                         {
-                             'rangeelectric',
-                             'rangeElectricKm',
-                             'criticalStateOfSoc',
-                             'maxrange',
-                             'stateOfChargeElectricPercent',
-                             'endofchargetime',
-                             'criticalStateOfDeparturetimesoc',
-                             'warninglowbattery',
-                             'electricconsumptionreset',
-                             'maxStateOfChargeElectricPercent',
-                             'supplybatteryvoltage',
-                             'electricChargingStatus',
-                             'chargingstatus',
-                             'soc',
-                             'showChargingErrorAndDemand',
-                             'electricconsumptionstart'}],
-
-    'auxheatstatus': ['auxheat status', None, "auxheat", 'auxheatstatus',
-                      'value', 'aux_heat',
-                      {
-                          'auxheatActive',
-                          'auxheatwarnings',
-                          'auxheatruntime',
-                          'auxheatwarningsPush',
-                          'auxheattimeselection',
-                          'auxheattime1',
-                          'auxheattime2',
-                          'auxheattime3'}],
-
-    'tanklevelpercent': ['Fuel Level', '%', "odometer", 'tanklevelpercent',
-                         'value', None, None],
-
-    'odometer': ['Odometer', 'Km', 'odometer', 'odo',
-                 'value', None,
-                 {
-                     'distanceReset',
-                     "distanceStart",
-                     "averageSpeedReset",
-                     "averageSpeedStart",
-                     "distanceZEReset",
-                     "drivenTimeZEReset",
-                     "drivenTimeReset",
-                     "drivenTimeStart",
-                     "ecoscoretotal",
-                     "ecoscorefreewhl",
-                     "ecoscorebonusrange",
-                     "ecoscoreconst",
-                     "ecoscoreaccel",
-                     "gasconsumptionstart",
-                     "gasconsumptionreset",
-                     "gasTankRange",
-                     "gasTankLevel",
-                     "liquidconsumptionstart",
-                     "liquidconsumptionreset",
-                     "liquidRangeSkipIndication",
-                     "rangeliquid",
-                     "serviceintervaldays",
-                     "tanklevelpercent",
-                     "tankReserveLamp",
-                     'batteryState'}],
-}
-
-
-DEFAULT_CACHE_PATH = '.mercedesme-token-cache'
-
-CONF_COUNTRY_CODE = 'country_code'
-CONF_ACCEPT_LANG = 'accept_lang'
-CONF_PIN = 'pin'
-CONF_EXCLUDED_CARS = 'excluded_cars'
-CONF_SAVE_CAR_DETAILS = 'save_car_details'
-
-DATA_MME = 'mercedesmeapi'
-DEFAULT_NAME = 'Mercedes ME'
-DOMAIN = 'mercedesmeapi'
+DATA_MME = "mercedesmeapi"
+DEFAULT_NAME = "Mercedes ME"
+DOMAIN = "mercedesmeapi"
 
 FEATURE_NOT_AVAILABLE = "The feature %s is not available for your car %s"
 
-NOTIFICATION_ID = 'mercedesmeapi_integration_notification'
-NOTIFICATION_TITLE = 'Mercedes me integration setup'
+NOTIFICATION_ID = "mercedesmeapi_integration_notification"
+NOTIFICATION_TITLE = "Mercedes me integration setup"
 
 SIGNAL_UPDATE_MERCEDESME = "mercedesmeapi_update"
-
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -237,8 +49,8 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(CONF_PASSWORD): cv.string,
         vol.Optional(CONF_SCAN_INTERVAL, default=30):
             vol.All(cv.positive_int, vol.Clamp(min=60)),
-        vol.Optional(CONF_COUNTRY_CODE, default='DE'): cv.string,
-        vol.Optional(CONF_ACCEPT_LANG, default='en_DE'): cv.string,
+        vol.Optional(CONF_COUNTRY_CODE, default="DE"): cv.string,
+        vol.Optional(CONF_ACCEPT_LANG, default="en_DE"): cv.string,
         vol.Optional(CONF_EXCLUDED_CARS, default=[]):
             vol.All(cv.ensure_list, [cv.string]),
         vol.Optional(CONF_PIN): cv.string,
@@ -249,9 +61,6 @@ CONFIG_SCHEMA = vol.Schema({
 
 def setup(hass, config):
     """Set up MercedesMe System."""
-
-    from custom_components.mercedesmeapi.oauth import MercedesMeOAuth
-    from custom_components.mercedesmeapi.apicontroller import Controller
 
     conf = config[DOMAIN]
 
@@ -283,15 +92,15 @@ def setup(hass, config):
                                 conf.get(CONF_EXCLUDED_CARS),
                                 conf.get(CONF_SAVE_CAR_DETAILS),
                                 conf.get(CONF_PIN),
-                                hass.config.path(''))
+                                hass.config.path(""))
 
     hass.data[DATA_MME] = MercedesMeHub(mercedesme_api)
 
-    discovery.load_platform(hass, 'sensor', DOMAIN, {}, config)
-    discovery.load_platform(hass, 'lock', DOMAIN, {}, config)
-    discovery.load_platform(hass, 'device_tracker', DOMAIN, {}, config)
-    discovery.load_platform(hass, 'switch', DOMAIN, {}, config)
-    discovery.load_platform(hass, 'binary_sensor', DOMAIN, {}, config)
+    discovery.load_platform(hass, "sensor", DOMAIN, {}, config)
+    discovery.load_platform(hass, "lock", DOMAIN, {}, config)
+    discovery.load_platform(hass, "device_tracker", DOMAIN, {}, config)
+    discovery.load_platform(hass, "switch", DOMAIN, {}, config)
+    discovery.load_platform(hass, "binary_sensor", DOMAIN, {}, config)
 
     def hub_refresh(event_time):
         """Call Mercedes me API to refresh information."""
@@ -306,12 +115,14 @@ def setup(hass, config):
 
     return True
 
+
 class MercedesMeHub(object):
     """Representation of a base MercedesMe device."""
 
     def __init__(self, data):
         """Initialize the entity."""
         self.data = data
+
 
 class MercedesMeEntity(Entity):
     """Entity class for MercedesMe devices."""
@@ -322,7 +133,7 @@ class MercedesMeEntity(Entity):
         """Initialize the MercedesMe entity."""
         self._data = data
         self._state = False
-        self._name = licenseplate + ' ' + sensor_name
+        self._name = licenseplate + " " + sensor_name
         self._internal_name = internal_name
         self._unit = unit
         self._vin = vin
@@ -343,8 +154,8 @@ class MercedesMeEntity(Entity):
     def device_retrieval_status(self):
         return self.get_car_value(self._feature_name,
                                   self._object_name,
-                                  'retrievalstatus',
-                                  'error')
+                                  "retrievalstatus",
+                                  "error")
 
     def update(self):
         """Get the latest data and updates the states."""
@@ -356,7 +167,7 @@ class MercedesMeEntity(Entity):
         self._state = self.get_car_value(self._feature_name,
                                          self._object_name,
                                          self._attrib_name,
-                                         'error')
+                                         "error")
 
         _LOGGER.debug("Updated %s %s", self._internal_name, self._state)
 
@@ -388,18 +199,18 @@ class MercedesMeEntity(Entity):
             "car": self._licenseplate,
             "retrievalstatus": self.get_car_value(self._feature_name,
                                                   self._object_name,
-                                                  'retrievalstatus',
-                                                  'error')
+                                                  "retrievalstatus",
+                                                  "error")
         }
         if self._extended_attributes is not None:
             for attrib in self._extended_attributes:
                 if self.get_car_value(
                         self._feature_name, attrib,
-                        'retrievalstatus', 'error') == 'VALID':
+                        "retrievalstatus", "error") == "VALID":
                     state[attrib] = self.get_car_value(self._feature_name,
                                                        attrib,
-                                                       'value',
-                                                       'error')
+                                                       "value",
+                                                       "error")
         return state
 
     @property
--- custom_components/mercedesmeapi/binary_sensor.py
+++ custom_components/mercedesmeapi/binary_sensor.py
@@ -5,16 +5,20 @@ For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/binary_sensor.mercedesme/
 """
 import logging
-import datetime
 
-from homeassistant.components.binary_sensor import (BinarySensorDevice)
-from custom_components.mercedesmeapi import (
-    DATA_MME, FEATURE_NOT_AVAILABLE, MercedesMeEntity, BINARY_SENSORS)
+from homeassistant.components.binary_sensor import BinarySensorDevice
+from custom_components.mercedesmeapi import DATA_MME, MercedesMeEntity
+from .const import BINARY_SENSORS
 
 _LOGGER = logging.getLogger(__name__)
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+
+async def async_setup_platform(hass, config, async_add_devices,
+                               discovery_info=None):
     """Setup the sensor platform."""
+    if discovery_info is None:
+        return
+
     data = hass.data[DATA_MME].data
 
     if not data.cars:
@@ -24,22 +28,23 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     devices = []
     for car in data.cars:
         for key, value in sorted(BINARY_SENSORS.items()):
-            if value[5] is None or getattr(car.features, value[5]) == True:
+            if value[5] is None or getattr(car.features, value[5]) is True:
                 device = MercedesMEBinarySensor(
-                            data, 
-                            key, 
-                            value[0], 
-                            car.finorvin, 
-                            value[1], 
-                            car.licenseplate,
-                            value[2],
-                            value[3],
-                            value[4],
-                            value[6])
+                    data,
+                    key,
+                    value[0],
+                    car.finorvin,
+                    value[1],
+                    car.licenseplate,
+                    value[2],
+                    value[3],
+                    value[4],
+                    value[6],
+                )
                 if device.device_retrieval_status() == "VALID":
                     devices.append(device)
 
-    add_devices(devices, True)
+    async_add_devices(devices, True)
 
 
 class MercedesMEBinarySensor(MercedesMeEntity, BinarySensorDevice):
@@ -49,4 +54,3 @@ class MercedesMEBinarySensor(MercedesMeEntity, BinarySensorDevice):
     def is_on(self):
         """Return the state of the binary sensor."""
         return self._state
-
